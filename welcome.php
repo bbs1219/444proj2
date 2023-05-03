0df841f43f2f40fb9cc858affca27b3392e7c0ee
root <root@localhost.localdomain>
2023-05-03 12:06:20 -0400

    moved repository, cloned it to the html folder

---
+++ welcome.php
@@ -0,0 +1,7 @@
+<h2>Welcome to the inventory system api test</h2>
+<a href="addItem.php">Add an Item</a>
+<a href="editItem.php">Edit an Item</a>
+<a href="deleteItem.php">Delete an Item</a>
+<a href="getItem.php">Get a single Item</a>
+<a href="getAllItems.php">Get all Items</a>
+
